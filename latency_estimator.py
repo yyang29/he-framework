41ac2fb891d5a75997b4ccb7d5d20ad5c46bdcb3
Yang Yang <yyang0729@gmail.com>
2024-02-19 15:14:00 -0800

    update.

--- latency_estimator.py
+++ latency_estimator.py
@@ -131,35 +131,59 @@ class LatencyEstimator:
       #  else
       #    store L limbs and twiddle factors
       num_limbs = platform_constants.POLYNOMIALS_PER_CIPHERTEXT * self.he_params.L
-      max_num_limbs_in_scratchpad = self._get_max_num_limbs_in_scratchpad()
-      if max_num_limbs_in_scratchpad <= 2 * self.he_params.L:
-        limbs_in_scratchpad = max_num_limbs_in_scratchpad // 2
-        intt_tf_sets_in_scratchpad = (max_num_limbs_in_scratchpad -
-                                      2 * self.he_params.L)
-        ntt_tf_sets_in_scratchpad = ...
+      num_limbs_available = self._get_max_num_limbs_in_scratchpad()
+      if num_limbs_available <= self.he_params.L:
+        intt_output_limbs_in_scratchpad = num_limbs_available
+        baseconv_output_limbs_in_scratchpad = 0
+        intt_tf_sets_in_scratchpad = 0
+        ntt_tf_sets_in_scratchpad = 0
+      elif num_limbs_available <= 2 * self.he_params.L:
+        intt_output_limbs_in_scratchpad = num_limbs_available // 2
+        baseconv_output_limbs_in_scratchpad = min(num_limbs_available // 2,
+                                                  self.he_params.L - 1)
+        intt_tf_sets_in_scratchpad = 0
+        ntt_tf_sets_in_scratchpad = 0
+      else:
+        intt_output_limbs_in_scratchpad = self.he_params.L
+        baseconv_output_limbs_in_scratchpad = self.he_params.L - 1
+        num_limbs_left = num_limbs_available - 2 * self.he_params.L
+        intt_tf_sets_in_scratchpad = max(min(num_limbs_left, self.he_params.L),
+                                         0)
+        num_limbs_left -= intt_tf_sets_in_scratchpad
+        ntt_tf_sets_in_scratchpad = max(
+            min(num_limbs_left, self.he_params.L - 1), 0)
+
+      logger.debug(
+          f"scratchpad allocation: "
+          f"intt output limbs - {intt_output_limbs_in_scratchpad}, "
+          f"baseconv output limbs - {baseconv_output_limbs_in_scratchpad}, "
+          f"intt tf sets - {intt_tf_sets_in_scratchpad}, "
+          f"ntt tf sets - {ntt_tf_sets_in_scratchpad}.")
 
       # Polynomial 0
-      latency_intt = self._estimate_ntt_new(num_limbs, num_limbs,
-                                            num_limbs - scratchpad_limbs,
-                                            num_limbs)
-      latency_base_conv = self._estimate_base_conv_new(
-          num_limbs, num_limbs - 1, num_limbs - scratchpad_limbs,
-          num_limbs - scratchpad_limbs, num_limbs - 1)
-      latency_ntt = self._estimate_ntt_new(num_limbs - 1,
-                                           num_limbs - scratchpad_limbs,
-                                           num_limbs - 1, num_limbs - 1)
+      p0_latency_intt = self._estimate_ntt_new(
+          num_limbs, num_limbs, num_limbs - intt_output_limbs_in_scratchpad,
+          num_limbs)
+      p0_latency_base_conv = self._estimate_base_conv_new(
+          num_limbs, num_limbs - 1, num_limbs - intt_output_limbs_in_scratchpad,
+          num_limbs - 1 - baseconv_output_limbs_in_scratchpad)
+      p0_latency_ntt = self._estimate_ntt_new(
+          num_limbs - 1, num_limbs - 1 - baseconv_output_limbs_in_scratchpad,
+          num_limbs - 1, num_limbs - 1)
+      p0_latency_us = p0_latency_intt + p0_latency_base_conv + p0_latency_ntt
       # Polynomial 1
-      latency_intt = self._estimate_ntt_new(num_limbs, num_limbs,
-                                            num_limbs - scratchpad_limbs,
-                                            num_limbs)
-      latency_base_conv = self._estimate_base_conv_new(
-          num_limbs, num_limbs - 1, num_limbs - scratchpad_limbs,
-          num_limbs - scratchpad_limbs, num_limbs - 1)
-      latency_ntt = self._estimate_ntt_new(num_limbs - 1,
-                                           num_limbs - scratchpad_limbs,
-                                           num_limbs - 1, num_limbs - 1)
+      p1_latency_intt = self._estimate_ntt_new(
+          num_limbs, num_limbs, num_limbs - intt_output_limbs_in_scratchpad,
+          num_limbs - intt_tf_sets_in_scratchpad)
+      p1_latency_base_conv = self._estimate_base_conv_new(
+          num_limbs, num_limbs - 1, num_limbs - intt_output_limbs_in_scratchpad,
+          num_limbs - 1 - baseconv_output_limbs_in_scratchpad)
+      p1_latency_ntt = self._estimate_ntt_new(
+          num_limbs - 1, num_limbs - 1 - baseconv_output_limbs_in_scratchpad,
+          num_limbs - 1, num_limbs - 1 - ntt_tf_sets_in_scratchpad)
+      p1_latency_us = p1_latency_intt + p1_latency_base_conv + p1_latency_ntt
 
-      latency_us = latency_intt + latency_base_conv + latency_ntt
+      latency_us = p0_latency_us + p1_latency_us
       logger.info(f"{self.op} takes {latency_us} us.")
 
       return latency_us
@@ -291,15 +315,19 @@ class LatencyEstimator:
 
   def _estimate_ntt_new(self, num_limbs, num_limbs_read, num_limbs_write,
                         num_tf_sets_read):
-    tf_read_bytes = utils.get_twiddle_factor_size_bytes(
-        self.he_params, num_limbs) * (1 - tf_reuse_ratio)
-    limb_read_bytes = 0 if input_fused else utils.get_limb_size_bytes(
-        self.he_params) * num_limbs
+    logger.debug(f"Estimating NTT/INTT latency. "
+                 f"num_limbs={num_limbs} "
+                 f"num_limbs_read={num_limbs_read} "
+                 f"num_limbs_write={num_limbs_write} "
+                 f"num_tf_sets_read={num_tf_sets_read}.")
+    tf_read_bytes = utils.get_twiddle_factor_size_bytes(self.he_params,
+                                                        num_tf_sets_read)
+    limb_read_bytes = utils.get_limb_size_bytes(self.he_params) * num_limbs_read
     total_read_bytes = tf_read_bytes + limb_read_bytes
     memory_read_time_us = total_read_bytes / self.constraints.bandwidth_gbps / 1e3
 
-    total_write_bytes = 0 if output_fused else utils.get_limb_size_bytes(
-        self.he_params) * num_limbs
+    total_write_bytes = utils.get_limb_size_bytes(
+        self.he_params) * num_limbs_write
     memory_write_time_us = total_write_bytes / self.constraints.bandwidth_gbps / 1e3
 
     # Factor of "1.5" in the alu_cycles to indicate the mult and add/sub require
@@ -318,16 +346,24 @@ class LatencyEstimator:
     return latency_us * self.derating_factor
 
   def _estimate_base_conv_new(self, num_input_limbs, num_output_limbs,
-                              num_limbs_read, num_limbs_write,
-                              num_compute_limbs):
+                              num_limbs_read, num_limbs_write):
+    # For base conversion, we store (num_input_limbs - num_limbs_read) limbs
+    # in the scratchpad. They are reused by num_output_limbs times.
+    # For the other limbs, they have to be streamed to DRAM once for each
+    # output limb.
+    logger.debug(f"Estimating base conversion latency. "
+                 f"num_input_limbs={num_input_limbs} "
+                 f"num_output_limbs={num_output_limbs} "
+                 f"num_limbs_read={num_limbs_read} "
+                 f"num_limbs_write={num_limbs_write}.")
     memory_read_time_us = (utils.get_limb_size_bytes(self.he_params) *
-                           num_input_limbs / self.constraints.bandwidth_gbps /
-                           1e3)
+                           num_limbs_read * num_output_limbs /
+                           self.constraints.bandwidth_gbps / 1e3)
     memory_write_time_us = (utils.get_limb_size_bytes(self.he_params) *
-                            num_output_limbs / self.constraints.bandwidth_gbps /
+                            num_limbs_write / self.constraints.bandwidth_gbps /
                             1e3)
     compute_time_us = (
-        2 * num_compute_limbs *
+        2 * num_output_limbs *
         (self.he_params.N / self.design_params.num_modular_alus) *
         utils.get_cycle_us(self.constraints.frequency_mhz))
 
